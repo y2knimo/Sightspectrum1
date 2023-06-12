8fbe7263790eb7ffd58b09d518bf8fd724df0cd8
y2knimo <135319860+y2knimo@users.noreply.github.com>
2023-06-12 16:10:49 +0530

    second  commit

---
+++ week3/week_3task_1/main.py
@@ -0,0 +1,13 @@
+languages = ['Swift', 'Python', 'Go']
+
+for language in languages:
+    print('Hello')
+    print('Hi')
+
+#for loop syntex
+
+languages = ['Swift', 'Python', 'Go', 'JavaScript']
+
+# run a loop for each item of the list
+for language in languages:
+    print(language)
