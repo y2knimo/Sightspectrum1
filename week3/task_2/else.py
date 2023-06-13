80c58507543c577e4ad3fbfecee4900babcec94e
y2knimo <135319860+y2knimo@users.noreply.github.com>
2023-06-13 16:56:34 +0530

    first commit

---
+++ week3/task_2/else.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    if fruit == "orange":
+        break
+    print(fruit)
+else:
+    print("No oranges found!")
---
+++ week3/task_2/first.py
@@ -0,0 +1,6 @@
+count = 0
+while count < 5:
+    count += 1
+    if count == 3:
+        continue 
+    print("Count:", count)
